3bb6caef19562d9e32ff626aea920be9193fed0d
Karl Hajjar <karl.hajjar@polytechnique.edu>
2021-05-07 19:22:47 +0200

    Added n_steps argument to mnist scripts command

--- scripts/server_scripts/abc_params/training/muP_multiple_act_mnist.sh
+++ scripts/server_scripts/abc_params/training/muP_multiple_act_mnist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-qsub -v activation="relu",dataset="mnist" scripts/server_scripts/abc_params/training/muP.sh &
-qsub -v activation="gelu",dataset="mnist" scripts/server_scripts/abc_params/training/muP.sh &
-qsub -v activation="elu",dataset="mnist" scripts/server_scripts/abc_params/training/muP.sh &
-qsub -v activation="tanh",dataset="mnist" scripts/server_scripts/abc_params/training/muP.sh &
-qsub -v activation="sigmoid",dataset="mnist" scripts/server_scripts/abc_params/training/muP.sh
+qsub -v activation="relu",dataset="mnist",n_steps=600 scripts/server_scripts/abc_params/training/muP.sh &
+qsub -v activation="gelu",dataset="mnist",n_steps=600 scripts/server_scripts/abc_params/training/muP.sh &
+qsub -v activation="elu",dataset="mnist",n_steps=600 scripts/server_scripts/abc_params/training/muP.sh &
+qsub -v activation="tanh",dataset="mnist",n_steps=600 scripts/server_scripts/abc_params/training/muP.sh &
+qsub -v activation="sigmoid",dataset="mnist",n_steps=600 scripts/server_scripts/abc_params/training/muP.sh
