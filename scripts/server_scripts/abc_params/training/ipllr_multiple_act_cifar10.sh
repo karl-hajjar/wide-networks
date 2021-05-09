de10ee51fed82a7c3c53d39c8c1e093140e75266
Karl Hajjar <karl.hajjar@polytechnique.edu>
2021-05-09 18:11:31 +0200

    Added more steps for cifar10

--- scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
+++ scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-qsub -v activation="relu",dataset="cifar10",n_steps=600 scripts/server_scripts/abc_params/training/ipllr.sh &
-qsub -v activation="gelu",dataset="cifar10",n_steps=600 scripts/server_scripts/abc_params/training/ipllr.sh &
-qsub -v activation="elu",dataset="cifar10",n_steps=600 scripts/server_scripts/abc_params/training/ipllr.sh &
-qsub -v activation="tanh",dataset="cifar10",n_steps=600 scripts/server_scripts/abc_params/training/ipllr.sh &
-qsub -v activation="sigmoid",dataset="cifar10",n_steps=600 scripts/server_scripts/abc_params/training/ipllr.sh
+qsub -v activation="relu",dataset="cifar10",n_steps=1200 scripts/server_scripts/abc_params/training/ipllr.sh &
+qsub -v activation="gelu",dataset="cifar10",n_steps=1200 scripts/server_scripts/abc_params/training/ipllr.sh &
+qsub -v activation="elu",dataset="cifar10",n_steps=1200 scripts/server_scripts/abc_params/training/ipllr.sh &
+qsub -v activation="tanh",dataset="cifar10",n_steps=1200 scripts/server_scripts/abc_params/training/ipllr.sh &
+qsub -v activation="sigmoid",dataset="cifar10",n_steps=1200 scripts/server_scripts/abc_params/training/ipllr.sh
