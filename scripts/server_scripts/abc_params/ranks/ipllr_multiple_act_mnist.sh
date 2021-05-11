bf6485ceef907520c8a58aaeaee39ed5fe1134df
Karl Hajjar <karl.hajjar@polytechnique.edu>
2021-05-12 00:20:13 +0200

    Commented out rank scripts which already run

--- scripts/server_scripts/abc_params/ranks/ipllr_multiple_act_mnist.sh
+++ scripts/server_scripts/abc_params/ranks/ipllr_multiple_act_mnist.sh
@@ -2,6 +2,6 @@
 
 qsub -v activation="relu",dataset="mnist" scripts/server_scripts/abc_params/ranks/ipllr.sh &
 # qsub -v activation="gelu",dataset="mnist" scripts/server_scripts/abc_params/ranks/ipllr.sh &
-qsub -v activation="elu",dataset="mnist" scripts/server_scripts/abc_params/ranks/ipllr.sh &
+qsub -v activation="elu",dataset="mnist" scripts/server_scripts/abc_params/ranks/ipllr.sh
 # qsub -v activation="tanh",dataset="mnist" scripts/server_scripts/abc_params/ranks/ipllr.sh &
 # qsub -v activation="sigmoid",dataset="mnist" scripts/server_scripts/abc_params/ranks/ipllr.sh
