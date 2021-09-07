7daf181f4b7572589545e49baad393949e9cf039
Karl Hajjar <karl.hajjar@polytechnique.edu>
2021-09-07 18:36:04 +0200

    Removed overriden methods in fc/muP.py

--- pytorch/models/abc_params/fully_connected/muP.py
+++ pytorch/models/abc_params/fully_connected/muP.py
@@ -1,9 +1,8 @@
-import torch.nn as nn
-
 from pytorch.models.abc_params.base_muP import BaseMuP
+from .ip import FCIP
 
 
-class FCmuP(BaseMuP):
+class FCmuP(BaseMuP, FCIP):
     """
     A class implementing the NTK parameterization of a fully-connected network.
     """
@@ -14,14 +13,11 @@ class FCmuP(BaseMuP):
         :param config: the configuration to define the network (architecture, loss, optimizer)
         :param width: the common width (number of neurons) of all layers except the last.
         """
-        super().__init__(config, width)
+        BaseMuP.__init__(self, config, width)
 
         if self._name == "model":
             self._name = "FCmuP"
         self._name = '{}_{}_{}'.format(self._name, self.width, self.n_layers)
-
-        # define an attribute to hold all the history of train/val/test metrics for later plotting /analysing
-        self.results = {'training': [], 'validation': [], 'test': []}
 
     def _build_model(self, config):
         """
@@ -29,40 +25,4 @@ class FCmuP(BaseMuP):
         :param config: the configuration defining the loss, optimizer and architecture.
         :return:
         """
-        # input layer
-        self.input_layer = nn.Linear(in_features=config.architecture["input_size"],
-                                     out_features=self.width,
-                                     bias=True)
-
-        # intermediate layers
-        self.intermediate_layers = nn.ModuleList()
-        for l in range(2, self.n_layers):
-            self.intermediate_layers.add_module("layer_{:,}_intermediate".format(l),
-                                                nn.Linear(in_features=self.width,
-                                                          out_features=self.width,
-                                                          bias=config.architecture["bias"]))
-
-        # output layer
-        self.output_layer = nn.Linear(in_features=self.width,
-                                      out_features=config.architecture["output_size"],
-                                      bias=config.architecture["bias"])
-
-    def predict(self, x, mode='probas', from_logits=False):
-        pass
-
-    def training_step(self, batch, batch_nb):
-        pass
-
-    # TODO : Apparently 'training_epoch_end' is not compatible with newer versions of PyTorch Lightning and might have
-    #  to be changed to some other name such as 'on_epoch_end' for compliance with versions >= 0.9.0.
-    def training_epoch_end(self, outputs):
-        pass
-
-    def _evaluation_step(self, batch, batch_nb, mode: str):
-        pass
-
-    def _evaluation_epoch_end(self, outputs, mode: str):
-        pass
-
-    def configure_optimizers(self):
-        pass
+        FCIP._build_model(self, config)
