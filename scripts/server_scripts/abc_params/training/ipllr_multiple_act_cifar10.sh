feccdf5c1abcfacc26d0709dd4d79959a3e451e1
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-01-26 19:37:20 +0100

    Changed argument passing to ipllr script. Also tried passing argument through defined env variable as a check.

--- scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
+++ scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
@@ -2,8 +2,8 @@
 
 n_steps=1200
 
-sbatch --export=activation="relu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-sbatch --export=activation="gelu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-sbatch --export=activation="elu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-sbatch --export=activation="tanh",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh
 # sbatch --export=activation="sigmoid",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
--- scripts/server_scripts/examples/example_param.sh
+++ scripts/server_scripts/examples/example_param.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo "Running sbatch with arguments n=3 and word='Karl'"
-sbatch scripts/server_scripts/examples/example.sh 3 "Karl"
+n=3
+
+echo "Running sbatch with arguments n=$n and word='Karl'"
+sbatch scripts/server_scripts/examples/example.sh $n "Karl"
