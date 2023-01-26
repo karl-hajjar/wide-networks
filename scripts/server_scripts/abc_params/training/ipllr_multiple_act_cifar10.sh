67fa1bd143b57b60f1bcbbe1a784c613c977303b
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-01-26 15:29:03 +0100

    Trying with --export= command in sbatch

--- scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
+++ scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
@@ -2,8 +2,8 @@
 
 n_steps=1200
 
-sbatch activation="relu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-sbatch activation="gelu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-sbatch activation="elu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-sbatch activation="tanh",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
-# sbatch activation="sigmoid",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
+sbatch --export=activation="relu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
+sbatch --export=activation="gelu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
+sbatch --export=activation="elu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
+sbatch --export=activation="tanh",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
+# sbatch --export=activation="sigmoid",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
