59983c291e06697152522b8ef68b68d1c7651072
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-01-26 19:27:45 +0100

    Trying one last time with named arguments

--- scripts/server_scripts/examples/example_param.sh
+++ scripts/server_scripts/examples/example_param.sh
@@ -3,4 +3,4 @@
 echo "Running sbatch with arguments word='Karl'"
 # sbatch --export=word="Karl" scripts/server_scripts/examples/example.sh
 # sbatch scripts/server_scripts/examples/example.sh
-sbatch scripts/server_scripts/examples/example.sh $word="Karl"
+sbatch scripts/server_scripts/examples/example.sh -word="Karl"
