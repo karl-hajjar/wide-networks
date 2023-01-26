6cc4f305b4ee7e066be8394ec56f598c3c4e4b01
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-01-26 20:10:42 +0100

    Trying one job only

--- scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
+++ scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
@@ -2,10 +2,10 @@
 
 n_steps=1200
 
-# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps
 
-sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
-sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
-sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
-sbatch scripts/server_scripts/abc_params/training/ipllr.sh
+# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
+# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
+# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
+# sbatch scripts/server_scripts/abc_params/training/ipllr.sh
 # sbatch --export=activation="sigmoid",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
