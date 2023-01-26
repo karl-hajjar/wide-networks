7cb256db9da42cdc62f0e1e03023e9e600c39b3b
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-01-26 15:24:52 +0100

    Fixed issue with command srun changed to sbatch in server script

--- scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
+++ scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
@@ -2,8 +2,8 @@
 
 n_steps=1200
 
-srun activation="relu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-srun activation="gelu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-srun activation="elu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
-srun activation="tanh",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
-# srun activation="sigmoid",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
+sbatch activation="relu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
+sbatch activation="gelu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
+sbatch activation="elu",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh &
+sbatch activation="tanh",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
+# sbatch activation="sigmoid",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
