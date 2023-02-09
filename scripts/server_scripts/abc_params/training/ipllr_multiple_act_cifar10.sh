371b9df59698f7922e22a96853cf79ff478cc2ec
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-02-09 17:17:26 +0100

    Reset script to multiple simultaneous experiments

--- scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
+++ scripts/server_scripts/abc_params/training/ipllr_multiple_act_cifar10.sh
@@ -2,10 +2,10 @@
 
 n_steps=1200
 
-sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps
+# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps
 
-# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
-# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "gelu" "cifar10" $n_steps &
-# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "elu" "cifar10" $n_steps &
-# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "tanh" "cifar10" $n_steps
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "cifar10" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "gelu" "cifar10" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "elu" "cifar10" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "tanh" "cifar10" $n_steps
 # sbatch --export=activation="sigmoid",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
