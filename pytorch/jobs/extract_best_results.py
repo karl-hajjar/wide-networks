2cb05d97ef93546d83d4c1012407c234f02f8e47
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-04-17 15:45:44 +0200

    Fixed issue with base experiment path in results extraction job

--- pytorch/jobs/extract_best_results.py
+++ pytorch/jobs/extract_best_results.py
@@ -3,7 +3,7 @@ import logging
 import click
 import numpy as np
 
-from utils.tools import load_pickle
+from utils.tools import load_pickle, set_up_logger
 
 
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -26,29 +26,35 @@ BEST_RESULTS_FILE = 'best_results.pickle'
 @click.option('--model', '-mod', required=False, type=click.STRING, default="ipllr",
               help='Which model was used in the experiment')
 def run(depth=5, width=1024, n_trials=5, dataset="mnist", model="ipllr"):
+    print(ROOT)
+    set_up_logger(os.path.join(ROOT, 'extract.log'))
     logger = logging.getLogger()
 
-    base_experiments_dir = os.path.join(ROOT, EXPERIMENTS_DIR, 'fc_{}_{}', 'L={}_m={}'.format(model, dataset, depth,
-                                                                                              width))
+    base_experiments_dir = os.path.join(ROOT, EXPERIMENTS_DIR, 'fc_{}_{}', 'L={}_m={}').format(model, dataset, depth,
+                                                                                               width)
     if not os.path.exists(base_experiments_dir):
         logger.warning("Path {} does not exits, stopping results extraction.".format(base_experiments_dir))
     else:
-        logger.info("Retrieving results from directory : {}".format(base_experiments_dir))
-        test_accuracy_by_lr_dict = dict()
-        for activation in ACTIVATIONS:
-            test_accuracy_by_lr_dict[activation] = dict()
-            for f in os.scandir(base_experiments_dir):
-                if activation in f.name:
-                    lr = float(f.name.split('lr=')[1].split('_')[0])
-                    accuracies = []
-                    for i in range(1, n_trials + 1):
-                        best_results_path = os.path.join(base_experiments_dir, f.name, 'trial_{}'.format(i),
-                                                         BEST_RESULTS_FILE)
-                        accuracies.append(load_pickle(best_results_path, single=True)['accuracy'])
-                    test_accuracy_by_lr_dict[activation][lr] = np.mean(accuracies)
-                    best_lr = max(test_accuracy_by_lr_dict, key=test_accuracy_by_lr_dict.get)
-                    logger.info("For activation {}: best lr = {} with accuracy = {:.5f}".
-                                format(activation, best_lr, test_accuracy_by_lr_dict[best_lr]))
+        try:
+            logger.info("Retrieving results from directory : {}".format(base_experiments_dir))
+            test_accuracy_by_lr_dict = dict()
+            for activation in ACTIVATIONS:
+                test_accuracy_by_lr_dict[activation] = dict()
+                for f in os.scandir(base_experiments_dir):
+                    if activation in f.name:
+                        lr = float(f.name.split('lr=')[1].split('_')[0])
+                        accuracies = []
+                        for i in range(1, n_trials + 1):
+                            best_results_path = os.path.join(base_experiments_dir, f.name, 'trial_{}'.format(i),
+                                                             BEST_RESULTS_FILE)
+                            accuracies.append(load_pickle(best_results_path, single=True)['accuracy'])
+                        test_accuracy_by_lr_dict[activation][lr] = np.mean(accuracies)
+                        best_lr = max(test_accuracy_by_lr_dict, key=test_accuracy_by_lr_dict.get)
+                        logger.info("For activation {}: best lr = {} with accuracy = {:.5f}".
+                                    format(activation, best_lr, test_accuracy_by_lr_dict[best_lr]))
+        except Exception as e:
+            logger.exception("Exception while running the best results extraction : {}".format(e))
+            raise Exception(e)
 
 
 if __name__ == '__main__':
