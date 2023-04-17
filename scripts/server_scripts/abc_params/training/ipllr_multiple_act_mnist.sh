f5ff6d389ed83e4a3d6252ef0a8a84b7c7a2aef4
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-04-17 11:21:09 +0200

    Setting up LRs for relu experiments

--- scripts/server_scripts/abc_params/training/ipllr_multiple_act_mnist.sh
+++ scripts/server_scripts/abc_params/training/ipllr_multiple_act_mnist.sh
@@ -2,9 +2,9 @@
 
 n_steps=5000
 
-# sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "mnist" $n_steps &
-sbatch scripts/server_scripts/abc_params/training/ipllr.sh "gelu" "mnist" $n_steps &
-sbatch scripts/server_scripts/abc_params/training/ipllr.sh "elu" "mnist" $n_steps &
-sbatch scripts/server_scripts/abc_params/training/ipllr.sh "tanh" "mnist" $n_steps
+sbatch scripts/server_scripts/abc_params/training/ipllr.sh "relu" "mnist" $n_steps &
+#sbatch scripts/server_scripts/abc_params/training/ipllr.sh "gelu" "mnist" $n_steps &
+#sbatch scripts/server_scripts/abc_params/training/ipllr.sh "elu" "mnist" $n_steps &
+#sbatch scripts/server_scripts/abc_params/training/ipllr.sh "tanh" "mnist" $n_steps
 # sbatch --export=activation="sigmoid",dataset="cifar10",n_steps=$n_steps scripts/server_scripts/abc_params/training/ipllr.sh
 
