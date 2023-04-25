cec520c3e4a7b7f1446dfdc70a9abf1b2bed1357
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-04-25 09:21:36 +0200

    Set up experiments for muP

--- scripts/server_scripts/abc_params/training/fc_mup_multiple_acts_mnist.sh
+++ scripts/server_scripts/abc_params/training/fc_mup_multiple_acts_mnist.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-n_steps=600
+n_steps=5000
 
-qsub -v activation="relu",dataset="mnist",n_steps=$n_steps scripts/server_scripts/abc_params/training/fc_mup.sh &
-qsub -v activation="gelu",dataset="mnist",n_steps=$n_steps scripts/server_scripts/abc_params/training/fc_mup.sh &
-qsub -v activation="elu",dataset="mnist",n_steps=$n_steps scripts/server_scripts/abc_params/training/fc_mup.sh &
-qsub -v activation="tanh",dataset="mnist",n_steps=$n_steps scripts/server_scripts/abc_params/training/fc_mup.sh
-# qsub -v activation="sigmoid",dataset="mnist",n_steps=$n_steps scripts/server_scripts/abc_params/training/standard_fc_ip.sh
+sbatch scripts/server_scripts/abc_params/training/fc_mup.sh "relu" "mnist" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/fc_mup.sh "gelu" "mnist" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/fc_mup.sh "elu" "mnist" $n_steps &
+sbatch scripts/server_scripts/abc_params/training/fc_mup.sh "tanh" "mnist" $n_steps
