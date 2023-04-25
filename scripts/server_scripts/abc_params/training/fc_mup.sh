29b38eab6f8ca25425c2769a21906648931b7184
Karl Hajjar <karl.hajjar@polytechnique.edu>
2023-04-25 11:13:45 +0200

    Trying again with [node..]

--- scripts/server_scripts/abc_params/training/fc_mup.sh
+++ scripts/server_scripts/abc_params/training/fc_mup.sh
@@ -27,4 +27,4 @@ export PYTHONPATH=$PYTHONPATH:"$PWD"  # add wide-networks library to python path
 # --mem=MB permet de définir le taille minimale de mémoire pour chaque processus.
 
 echo "Launching Python script from bash with srun..."
-srun -N1 --nodelist=[11] python3 pytorch/jobs/abc_parameterizations/fc_mup_run.py --activation=$1 --dataset=$2 --n_steps=$3
+srun -N1 --nodelist=[node11] python3 pytorch/jobs/abc_parameterizations/fc_mup_run.py --activation=$1 --dataset=$2 --n_steps=$3
